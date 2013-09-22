b3a1321a5e7ca18353f8584a2380b561f4120e92
Mark Needham <m.h.needham@gmail.com>
2013-09-22 10:02:43 +0100

    removing unused code

--- src/main/java/org/neo4j/dataimport/Neo4jServer.java
+++ src/main/java/org/neo4j/dataimport/Neo4jServer.java
@@ -1,43 +1,27 @@
 package org.neo4j.dataimport;
 
 
+import com.googlecode.totallylazy.Callable1;
+import com.googlecode.totallylazy.Pair;
+import com.googlecode.totallylazy.Sequence;
+import org.apache.commons.lang.StringUtils;
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.graphdb.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.core.MediaType;
-
-import com.googlecode.totallylazy.Callable1;
-import com.googlecode.totallylazy.Pair;
-import com.googlecode.totallylazy.Sequence;
-import com.sun.jersey.api.client.ClientResponse;
-import org.apache.commons.lang.StringUtils;
-import org.codehaus.jackson.node.ArrayNode;
-import org.codehaus.jackson.node.JsonNodeFactory;
-import org.codehaus.jackson.node.ObjectNode;
-
-import org.neo4j.cypher.javacompat.ExecutionEngine;
-import org.neo4j.graphdb.*;
-
-import static com.googlecode.totallylazy.numbers.Numbers.range;
-
 public class Neo4jServer
 {
-    private static final String NODE_LOOKUP = "node%s = node({%s}), node%s=node({%s})";
-    private static final String CREATE_RELATIONSHIP = " CREATE %s-[:%s]->%s";
-
     private final GraphDatabaseService db;
     private final int batchSize;
-    private int batchWithinBatchSize;
-    private ExecutionEngine executionEngine;
 
     public Neo4jServer( GraphDatabaseService db, int batchSize )
     {
         this.db = db;
         this.batchSize = batchSize;
-        this.batchWithinBatchSize = 50;
-        this.executionEngine = new ExecutionEngine( db );
     }
 
     public Map<String, Long> importNodes( List<Map<String, Object>> nodes )
@@ -70,8 +54,6 @@ public class Neo4jServer
 
         for ( int i = 0; i < numberOfRelationshipsToImport; i += batchSize ) {
             Sequence<Map<String, Object>> batchRels = relationships.drop( i ).take( batchSize );
-//            int numberOfNodes = batchSize * 2;
-//            Sequence<Pair<Number, Number>> nodePairs = range( 1, numberOfNodes - 1, 2 ).zip( range( 2, numberOfNodes, 2 ) );
 
             Transaction tx = db.beginTx();
 
@@ -87,83 +69,8 @@ public class Neo4jServer
 
             tx.success();
             tx.finish();
-
-//            String query = "START ";
-//            query += StringUtils.join( nodePairs.zip( relationships ).map( nodeLookup() ).iterator(), ", " );
-//            query += StringUtils.join( relationships.zip( nodePairs ).map( createRelationship() ).iterator(), " " );
-//
-//            Map<String, Object> parameters = createParametersFrom( nodeParameterMappings( nodePairs.zip( batchRels ), nodeIdMappings ) );
-//
-//            executionEngine.execute( query, parameters );
         }
     }
 
-    private String createQuery( Sequence<Map<String, Object>> relationships, Sequence<Pair<Number, Number>> nodePairs )
-    {
-        String query = "START ";
-        query += StringUtils.join( nodePairs.zip( relationships ).map( nodeLookup() ).iterator(), ", " );
-        query += StringUtils.join( relationships.zip( nodePairs ).map( createRelationship() ).iterator(), " " );
-        return query;
-    }
-
-    private Map<String, Object> createParametersFrom( List<Pair<Number, Number>> relationshipMappings )
-    {
-        Map<String, Object> params = new HashMap<String, Object>();
-
-        for ( Pair<Number, Number> relationshipMapping : relationshipMappings )
-        {
-            params.put( relationshipMapping.first().toString(), relationshipMapping.second().longValue() );
-        }
-        return params;
-    }
-
-
-    private List<Pair<Number, Number>> nodeParameterMappings( final Sequence<Pair<Pair<Number, Number>, Map<String,
-            Object>>> relationshipMappings, Map<String,
-            Long> nodeIdMappings )
-    {
-        List<Pair<Number, Number>> pairs = new ArrayList<Pair<Number, Number>>();
-        for ( Pair<Pair<Number, Number>, Map<String, Object>> sequenceIdsToRelationship : relationshipMappings )
-        {
-            final Map<String, Object> relationship = sequenceIdsToRelationship.second();
-            Pair<Number, Number> sequenceIds = sequenceIdsToRelationship.first();
-
-            Long from = nodeIdMappings.get( relationship.get( "from" ).toString() );
-            Long to = nodeIdMappings.get( relationship.get( "to" ).toString() );
-
-            pairs.add( Pair.<Number, Number>pair( sequenceIds.first(), from ) );
-            pairs.add( Pair.<Number, Number>pair( sequenceIds.second(), to ) );
-        }
-        return pairs;
-    }
-
-    private Callable1<? super Pair<Map<String, Object>, Pair<Number, Number>>, ?> createRelationship()
-    {
-        return new Callable1<Pair<Map<String, Object>, Pair<Number, Number>>, Object>()
-        {
-            public Object call( Pair<Map<String, Object>, Pair<Number, Number>> mapPairPair ) throws Exception
-            {
-                String sourceNode = "node" + mapPairPair.second().first();
-                String destinationNode = "node" + mapPairPair.second().second();
-                String relationshipType = mapPairPair.first().get( "type" ).toString();
-                return String.format( CREATE_RELATIONSHIP, sourceNode, relationshipType, destinationNode );
-            }
-        };
-    }
-
-    private Callable1<Pair<Pair<Number, Number>, Map<String, Object>>, Object> nodeLookup()
-    {
-        return new Callable1<Pair<Pair<Number, Number>, Map<String, Object>>, Object>()
-        {
-            public Object call( final Pair<Pair<Number, Number>, Map<String, Object>> pairMapPair ) throws Exception
-            {
-                return String.format( NODE_LOOKUP,
-                        pairMapPair.first().first(),
-                        pairMapPair.first().first(),
-                        pairMapPair.first().second(),
-                        pairMapPair.first().second() );
-            }
-        };
-    }
 
 }
